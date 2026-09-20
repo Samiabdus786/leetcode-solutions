c03adcbdaa458a35acb8408555a322eb611c2c8b
Samiabdus786 <samiabdus9831@gmail.com>
2026-09-20 23:08:15 +0530

    Time: 0 ms (100%), Space: 19.4 MB (30.49%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -0,0 +1,11 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        st = []
+        d = {}
+
+        for x in nums2:
+            while st and st[-1] < x:
+                d[st.pop()] = x
+            st.append(x)
+
+        return [d.get(x, -1) for x in nums1]
